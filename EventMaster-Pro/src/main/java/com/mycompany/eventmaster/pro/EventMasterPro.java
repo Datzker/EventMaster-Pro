86f656216b62bb86bed06f5382ad071b1388c1a9
Datzker <danielthc601@gmail.com>
2025-04-27 18:45:51 -0500

    Creación del proyecto en NetBeans

---
+++ EventMaster-Pro/src/main/java/com/mycompany/eventmaster/pro/EventMasterPro.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eventmaster.pro;
+
+/**
+ *
+ */
+public class EventMasterPro {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
